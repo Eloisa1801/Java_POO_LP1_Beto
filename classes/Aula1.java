f70d988564fd45d6e6eb71a1cbe6dce25bd8e1f2
Paulo-vca <paulovca26@gmail.com>
2022-07-27 15:43:16 -0400

    subindo

---
+++ classes/Aula1.java
@@ -0,0 +1,38 @@
+package classes;
+
+
+public class Aula1{
+     
+    private int j = 0; // e um atributo
+
+    public static void main(String[] args){
+        int i = 0; // i e var do metodo main
+        
+        Cachorro doguinho = new Cachorro(); //objeto doguinho
+                                //Construtor
+    }
+
+
+    private void soma(int a, int b ){
+        int i = 2; //i e var do metodo soma
+        int soma = a + b + i + j;
+
+    }
+
+}
+
+// Midificador de Acesso
+
+
+//  + public > Aberto para todos
+
+//  - private > visivel apeneas na propria classe  - encapsulamento
+
+        // * Atributo
+        // * Metodo
+        // * Classe  > 
+
+//  # protected > herança
+
+//  ~ package >
+
---
+++ classes/Cachorro.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Cachorro {
+
+    private String nome;
+    private int idade;
+    private String raca;
+    private boolean dono;
+    private boolean castrado;
+    private float peso;
+    
+
+}
